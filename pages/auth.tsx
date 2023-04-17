f32f6f29f3f9a47b7b978b5682bffb38b5114b20
Nyamosiyego <edmond.nyamosi@strathmore.edu>
2023-04-17 10:16:19 +0300

    .

---
+++ pages/auth.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const Auth = (props: Props) => {
+  return (
+    <div className="relative h-full w-full bg-[url('/images/hero.jpg')]"></div>
+  );
+}
+
+export default Auth
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1>Netflix Clone</h1>
+      <h1 className="">Netflix Clone</h1>
     </div>
   )
 }
